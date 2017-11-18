3fd48756adb157a3adf51d1d48e76b8e207930c3
Carleslc <lazaro.costa.carles@gmail.com>
2017-11-18 14:33:32 +0100

    restore_cache process all rows

--- Entrega2/tweet_location_preprocessing/restore_cache.py
+++ Entrega2/tweet_location_preprocessing/restore_cache.py
@@ -11,17 +11,21 @@ PRINTABLE = set(string.printable)
 CACHE_FILE = 'gender-classifier-tweet-location-preprocessed-cache.json'
 CSV = 'gender-classifier-tweet-location-preprocessed-0-18933.csv'
 
-ROW_START = 0
-ROW_END = 1205
-
 twigen = pd.read_csv("../gender-classifier-DFE-791531.csv", encoding=ENCODING)
 twigen = twigen.drop(twigen[twigen.gender == 'unknown'].index)
 
 def is_printable(character):
     return character in PRINTABLE
 
-tweet_location = twigen[ROW_START : ROW_END].tweet_location.apply(lambda location: ''.join(filter(is_printable, str(location).lower())).strip()).as_matrix()
-geolocation_country = pd.read_csv(CSV, encoding=ENCODING)[ROW_START : ROW_END].tweet_location.as_matrix()
+confirmation = input(f'Se modificará el fichero {CACHE_FILE} con los datos procesados de tweet_location de {CSV}.\
+    \n¿Estás seguro? [y/N] ')
+
+if confirmation != 'y':
+    print('Cancelled')
+    exit()
+
+tweet_location = twigen.tweet_location.apply(lambda location: ''.join(filter(is_printable, str(location).lower())).strip()).as_matrix()
+geolocation_country = pd.read_csv(CSV, encoding=ENCODING).tweet_location.as_matrix()
 
 print(tweet_location)
 print(geolocation_country)
